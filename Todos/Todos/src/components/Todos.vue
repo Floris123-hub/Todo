9aa3e8dc6f10dac80054fc0e9203e537ee6b1699
Floris <floriscapo@gmail.com>
2020-12-04 16:57:42 +0100

    update

--- Todos/Todos/src/components/Todos.vue
+++ Todos/Todos/src/components/Todos.vue
@@ -33,79 +33,79 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        todos: [],
-        newTodo: '',
-        filter: 'all',
-        editing: null,
-        oldTodo: ''
+export default {
+  data () {
+    return {
+      todos: [],
+      newTodo: '',
+      filter: 'all',
+      editing: null,
+      oldTodo: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      })
+      this.newTodo = ''
+    },
+    deleteTodo (todo) {
+      this.todos = this.todos.filter(i => i !== todo)
+    },
+    deleteCompleted (todo) {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    },
+    editTodo (todo) {
+      this.editing = todo
+      this.oldTodo = todo.name
+    },
+    doneEdit () {
+      this.editing = null
+    },
+    cancelEdit () {
+      this.editing.name = this.oldTodo
+      this.doneEdit()
+    }
+  },
+  computed: {
+    allDone: {
+      get () {
+        return this.remaining === 0
+      },
+      set (value) {
+        this.todos.forEach(function (todo) {
+          todo.completed = value
+        })
       }
     },
-    methods: {
-      addTodo() {
-        this.todos.push({
-          completed: false,
-          name: this.newTodo
-        });
-        this.newTodo = ''
-      },
-      deleteTodo(todo) {
-        this.todos = this.todos.filter(i => i !== todo)
-      },
-      deleteCompleted(todo) {
-        this.todos = this.todos.filter(todo => !todo.completed)
-      },
-      editTodo(todo) {
-        this.editing = todo;
-        this.oldTodo = todo.name
-      },
-      doneEdit() {
-        this.editing = null
-      },
-      cancelEdit() {
-        this.editing.name = this.oldTodo;
-        this.doneEdit()
-      }
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
     },
-    computed: {
-      allDone: {
-        get() {
-          return this.remaining === 0
-        },
-        set(value) {
-          this.todos.forEach(function (todo) {
-            todo.completed = value
-          })
-        }
-      },
-      remaining() {
-        return this.todos.filter(todo => !todo.completed).length
-      },
-      completed() {
-        return this.todos.filter(todo => todo.completed).length
-      },
-      filteredTodos() {
-        if (this.filter === 'todo') {
-          return this.todos.filter(todo => !todo.completed)
-        } else if (this.filter === 'done') {
-          return this.todos.filter(todo => todo.completed)
-        }
-        return this.todos
-      },
-      hasTodos() {
-        return this.todos.length > 0
-      }
+    completed () {
+      return this.todos.filter(todo => todo.completed).length
     },
-    directives: {
-      focus(el, value) {
-        if (value) {
-          el.focus()
-        }
+    filteredTodos () {
+      if (this.filter === 'todo') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter === 'done') {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos
+    },
+    hasTodos () {
+      return this.todos.length > 0
+    }
+  },
+  directives: {
+    focus (el, value) {
+      if (value) {
+        el.focus()
       }
     }
   }
+}
 </script>
 
 <style src="./todos.css"></style>
